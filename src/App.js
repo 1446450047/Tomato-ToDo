432b269d13e1144012daee26bbf07761b77c45ea
1446450047 <1446450047@qq.com>
2022-02-07 21:28:28 +0800

    添加全局css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import * as React from "react";
-
+import styled from "styled-components"
 function App() {
     return (
         <div className="wrapper">
-            <header></header>
+            <Header>header</Header>
         </div>
     );
 }
 
+const Header = styled.header`
+border: 1px solid red;
+`
 
 export default App;
