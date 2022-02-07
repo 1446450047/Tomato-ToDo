01cabb579cc307c89efc4c39c9177f4b26af621c
1446450047 <1446450047@qq.com>
2022-02-07 22:32:08 +0800

    将视图独立出来

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
 import * as React from "react";
 import Wrapper from "./components/wrapper";
 import Header from "./components/header/Header";
-import {Route, Routes} from "react-router";
-import Home from "./components/main/Home";
-import Tomato from "./components/main/Tomato";
-import ToDoList from "./components/main/ToDoList";
-import Record from "./components/main/Record";
-import Info from "./components/main/Info";
+import MainView from "./routes";
 
 function App() {
     return (
         <Wrapper className="wrapper">
             <Header/>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="tomato" element={<Tomato />} />
-                <Route path="todoList" element={<ToDoList />} />
-                <Route path="record" element={<Record />} />
-                <Route path="info" element={<Info />} />
-
-            </Routes>
+            <MainView/>
         </Wrapper>
     );
 }
---
+++ src/routes/index.jsx
@@ -0,0 +1,20 @@
+import {Route, Routes} from "react-router";
+import Home from "../components/main/Home";
+import Tomato from "../components/main/Tomato";
+import ToDoList from "../components/main/ToDoList";
+import Record from "../components/main/Record";
+import Info from "../components/main/Info";
+import * as React from "react";
+
+export default function MainView(){
+    return(
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="tomato" element={<Tomato />} />
+            <Route path="todoList" element={<ToDoList />} />
+            <Route path="record" element={<Record />} />
+            <Route path="info" element={<Info />} />
+
+        </Routes>
+    )
+}
