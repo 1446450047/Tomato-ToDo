51d682a23ad5d0324ebeb8621017b90da4f3842e
1446450047 <1446450047@qq.com>
2022-02-09 11:29:58 +0800

    引入study和main的基本布局

--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -1,10 +1,32 @@
 import React from "react";
-
+import study from "../../images/study.png"
+import styled from "styled-components";
 function Home(){
     return(
         <div>
-            <h1>home</h1>
+            <TextAndImage>
+                <div className="textInfo">文字信息</div>
+                <img src={study} alt="study"/>
+            </TextAndImage>
+            <Buttons>
+                <button>番茄</button>
+                <button>ToDo</button>
+                <button>历史数据</button>
+            </Buttons>
         </div>
     )
 }
+const TextAndImage = styled.div`
+  border: 1px solid red;
+  display: flex;
+  div{
+    width: 50%;
+  }
+  img{
+    width: 50%;
+  }
+`
+const Buttons = styled.div`
+  border: 1px solid blue;
+`
 export default Home
