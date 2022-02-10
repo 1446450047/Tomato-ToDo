c6a9a35c2273dab187acb4a902f9de5425141a29
1446450047 <1446450047@qq.com>
2022-02-10 13:30:04 +0800

    引入react-markdown

--- src/components/main/Info.jsx
+++ src/components/main/Info.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-
+import ReactMarkdown from 'react-markdown'
+import InfoText from "./Info.md"
 function Info(){
+    console.dir(InfoText);
     return(
         <div>
             <h2>开发日志</h2>
+            <ReactMarkdown># Hello, *world*!</ReactMarkdown>
         </div>
     )
 }
