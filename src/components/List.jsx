c27f4a8afa1b8f965114d84a83236f544c236cfc
1446450047 <1446450047@qq.com>
2022-02-11 18:16:53 +0800

    list中数据渲染

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,18 +1,18 @@
 import styled from "styled-components";
 import React from "react";
 
-export default function List({lists}){
-    console.log(lists);
+export default function List({lists}) {
+
     return (
         <ListWrapper>
-        {/*    lists.map((item)=>{*/}
-        {/*    <li>item</li>*/}
-        {/*})*/}
-        </ListWrapper>)
+            {lists.map((item) =>
+                <li key={item.id} className={item.done ? "finish" :"willDo" } >{item.content} ----- {item.createTime}</li>)}
+
+        </ListWrapper>);
 }
 
 const ListWrapper = styled.ul`
   border: 1px solid red;
   width: 60%;
   margin: 0 auto;
-`
+`;
