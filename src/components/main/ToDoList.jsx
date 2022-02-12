a430d3eda26d16dfcc6378967ff1d3e9b864b438
1446450047 <1446450047@qq.com>
2022-02-12 11:23:53 +0800

    独立出数据

--- src/components/main/ToDoList.jsx
+++ src/components/main/ToDoList.jsx
@@ -3,33 +3,7 @@ import styled from "styled-components";
 import Icon from "../header/Icon";
 import {Button} from "../Button";
 import List from "../List";
-
-const todoRecord = [
-    {
-        id: 1,
-        content: "吃雪糕",
-        createTime: "time",
-        done: true,
-    },
-    {
-        id: 2,
-        content: "吃雪糕",
-        createTime: "time",
-        done: false,
-    },
-    {
-        id: 3,
-        content: "吃雪糕",
-        createTime: "time",
-        done: true,
-    },
-    {
-        id: 4,
-        content: "吃雪糕",
-        createTime: "time",
-        done: false,
-    }
-];
+import {todoRecord} from "../../data";
 
 function ToDoList() {
     return (
---
+++ src/data.js
@@ -0,0 +1,29 @@
+
+const todoRecord = [
+    {
+        id: 1,
+        content: "吃雪糕",
+        createTime: "time",
+        done: true,
+    },
+    {
+        id: 2,
+        content: "吃雪糕",
+        createTime: "time",
+        done: false,
+    },
+    {
+        id: 3,
+        content: "吃雪糕",
+        createTime: "time",
+        done: true,
+    },
+    {
+        id: 4,
+        content: "吃雪糕",
+        createTime: "time",
+        done: false,
+    }
+];
+
+export {todoRecord}
