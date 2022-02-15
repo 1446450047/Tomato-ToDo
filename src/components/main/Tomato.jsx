c0dfec49f2e4462b5503a546c0674ebf8f26f4dc
1446450047 <1446450047@qq.com>
2022-02-15 21:32:32 +0800

    添加tomato 部分样式

--- src/components/main/Tomato.jsx
+++ src/components/main/Tomato.jsx
@@ -10,13 +10,116 @@ function Tomato() {
                 <span>:</span>
                 <span>00</span>
             </Timing>
-            <div>
+            <ButtonGroup>
                 <Button>开启一个番茄</Button>
                 <Button>休息一个间隙</Button>
-            </div>
+            </ButtonGroup>
             <h3>今日番茄</h3>
             <TomatoLists>
-                列表
+                <tr>
+                    <th>序号</th>
+                    <th>类型</th>
+                    <th>开始时间</th>
+                    <th>结束时间</th>
+                    <th>用时</th>
+                    <th>是否完成</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>番茄</td>
+                    <td>21：23</td>
+                    <td>21：33</td>
+                    <td>10</td>
+                    <td>否</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>休息</td>
+                    <td>21：43</td>
+                    <td>21：49</td>
+                    <td>6</td>
+                    <td>是</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>番茄</td>
+                    <td>21：23</td>
+                    <td>21：33</td>
+                    <td>10</td>
+                    <td>否</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>休息</td>
+                    <td>21：43</td>
+                    <td>21：49</td>
+                    <td>6</td>
+                    <td>是</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>番茄</td>
+                    <td>21：23</td>
+                    <td>21：33</td>
+                    <td>10</td>
+                    <td>否</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>休息</td>
+                    <td>21：43</td>
+                    <td>21：49</td>
+                    <td>6</td>
+                    <td>是</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>番茄</td>
+                    <td>21：23</td>
+                    <td>21：33</td>
+                    <td>10</td>
+                    <td>否</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>休息</td>
+                    <td>21：43</td>
+                    <td>21：49</td>
+                    <td>6</td>
+                    <td>是</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>番茄</td>
+                    <td>21：23</td>
+                    <td>21：33</td>
+                    <td>10</td>
+                    <td>否</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>休息</td>
+                    <td>21：43</td>
+                    <td>21：49</td>
+                    <td>6</td>
+                    <td>是</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>番茄</td>
+                    <td>21：23</td>
+                    <td>21：33</td>
+                    <td>10</td>
+                    <td>否</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>休息</td>
+                    <td>21：43</td>
+                    <td>21：49</td>
+                    <td>6</td>
+                    <td>是</td>
+                </tr>
             </TomatoLists>
         </TomatoWrapper>
     );
@@ -30,13 +133,37 @@ const Timing = styled.div`
     font-size: 120px;
     color: #F76E11;
     user-select: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+const ButtonGroup = styled.div`
+    display: flex;
+    justify-content: space-around;
+    padding: 60px 0;
+    div{
+      background:#F76E11;
+      color: #fff;
+      &:hover{
+         background:#ef8137;
+      }
+    }
 `;
 const TomatoWrapper = styled.div`
   border: 1px solid red;
   width: 60%;
   margin: 60px auto;
+  h3{
+      text-align: center;
+  }
 `;
 const TomatoLists = styled.table`
-  border: 1px solid blue;
+  border: 2px solid #F8802E;
+  border-radius: 4px;
+  margin: 0 auto;
+  padding: 12px 18px;
+  tr{
+    padding: 5px;
+  }
 `;
 export default Tomato;
