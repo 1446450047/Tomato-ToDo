a3a8dc8f679b89899538613a620c20e269b0676c
1446450047 <1446450047@qq.com>
2022-03-08 15:05:25 +0800

    引入echars

--- src/components/main/Record.jsx
+++ src/components/main/Record.jsx
@@ -1,10 +1,46 @@
-import React from "react";
+import React, {useEffect} from "react";
+import * as echarts from "echarts";
+import styled from "styled-components";
 
-function Record(){
-    return(
+function Record() {
+    const content = React.useRef();
+    useEffect(() => {
+        let myChart = echarts.init(content.current);
+        const option = {
+            title:{
+                text:"番茄工作法历史统计",
+                left:"center",
+                top:"20px"
+            },
+            xAxis: {
+                type: 'category',
+                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [
+                {
+                    data: [150, 230, 224, 218, 135, 147, 260],
+                    type: 'line',
+                    smooth: true
+                }
+            ]
+        }
+        myChart.setOption(option);
+    });
+
+    return (
         <div>
-            <h1>Record</h1>
+            <TomatoRecord ref={content}>
+
+            </TomatoRecord>
         </div>
-    )
+    );
 }
-export default Record
+const TomatoRecord = styled.div`
+  border: 1px solid red;
+  height: 400px;
+`
+export default Record;
+
